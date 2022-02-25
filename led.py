e923e95ebdafdc4f92180f6889856672986b6f3c
Federico Foschini <ffoschini@certego.net>
2022-02-25 14:03:59 +0100

    first commit

---
+++ led.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from gpiozero import LED
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/led", methods=['GET', 'POST'])
+def led():
+    led = LED(2, initial_value=None)
+    led_status = "on" if led.is_lit() else "off"
+    if request.method == 'POST':
+        led_status = request.form['led_status']
+        if led_status == "on":
+            led.on()
+        else:
+            led.off()
+
+    return render_template('led.html', led_status=led_status)
+
+
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from . import led
+
+if __name__ == "__main__":
+    led.app.run()
